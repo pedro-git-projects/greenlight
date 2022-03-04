702dcd526dbcfbdecc17e3115ff0611c63a80adc
pedro-git-projects <pedro.generic.email@gmail.com>
2022-03-04 10:45:37 -0300

    JSON writer helper func

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -13,18 +12,10 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"version":     version,
 	}
 
-	// Marshaling the map to JSON.
-	js, err := json.Marshal(data)
+	err := app.WriteJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
 		return
 	}
-
-	// appending a newline is just cosmetic, it will make it easier to read in the terminal
-	js = append(js, '\n')
-
-	// set content type and write response
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -17,4 +18,30 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+/* Defining a writeJSON() helper for sending responses.
+*  It takes the destination http.ResponseWriter, the status code to send, the data to be mashaled and
+*  a header map containing additional headers we might want to send in the response */
+func (app *application) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// apending newline for ease of read
+	js = append(js, '\n')
+
+	// iterating over headers map and adding to the response writer header map
+	// note that iterating trough an nil map won't cause errors
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	// Adding the Content-Type and writing headers and response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
