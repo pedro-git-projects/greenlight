feda1f1fcd06f61dacb02742c72ff87e5121a2d2
pedro-git-projects <pedro.generic.email@gmail.com>
2022-03-04 15:00:40 -0300

    decoding JSON into structs

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -8,9 +9,26 @@ import (
 	"github.com/pedro-git-projects/greenlight/internal/data"
 )
 
-// placeholder for movie creation
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	// Anonymous struct to hold the information we expect to recieve in the request body
+	// Its fields are a subset of our Movie struct
+	// This struct will be our target decode destination
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	// Decoding from http to struct
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// Dumping the contents of the input struct into an HTTP response
+	fmt.Fprintf(w, "%v+\n", input)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
