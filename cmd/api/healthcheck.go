a392bc18529e50dd38354ae5a321de4a91c7a379
pedro-git-projects <pedro.generic.email@gmail.com>
2022-03-04 10:14:30 -0300

    returning JSON in healthcheck

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,12 +5,16 @@ import (
 	"net/http"
 )
 
-// Handler writing plain-text response with information about
-// application status, environment setting and version
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	// not accessing trough the application struct because its currently hardcoded
-	fmt.Fprintf(w, "version: %s\n", version)
+	// Creating a fixed-format JSON response from a string.
+	// Raw string literals are used so as to include double-quote without needing to scape
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
 
+	// Setting "Content-Type: application/json" in the header of our response.
+	// this overrides the default "text/plain" "charset=utf-8"
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write said JSON to response body
+	w.Write([]byte(js))
 }
