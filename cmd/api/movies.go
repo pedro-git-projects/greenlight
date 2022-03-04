de51676668ebdd434aaf1b1f2c9d1826795b09f4
pedro-git-projects <pedro.generic.email@gmail.com>
2022-03-04 12:32:27 -0300

    writing errors in JSON

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -16,7 +16,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -33,8 +33,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	// Marshaling to json and sending as response
 	err = app.WriteJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 
 }
